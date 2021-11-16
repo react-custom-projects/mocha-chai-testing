8458e8140c8c4bb5e67b0790e427f61410e1fed0
Adam Morsi <adam.morsi@yahoo.com>
2021-11-16 15:17:59 +0200

    test input

--- src/js/containers/CurrentCounter.jsx
+++ src/js/containers/CurrentCounter.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CurrentCounter = ({ counter }) => {
-	return <p>Current counter: {counter}</p>;
+	const [name, setName] = useState('');
+
+	const nameHandler = ({ target: { value } }) => {
+		setName(value);
+	};
+
+	return (
+		<>
+			<input
+				data-test="name"
+				type="text"
+				onChange={nameHandler}
+				value={name}
+				placeholder="Enter a name"
+			/>
+			<p data-test="displayName">Name is: {name}</p>
+			<p>Current counter: {counter}</p>
+		</>
+	);
 };
 
 export default CurrentCounter;
--- src/test/containers/CurrentCounter.test.js
+++ src/test/containers/CurrentCounter.test.js
@@ -6,12 +6,21 @@ import CurrentCounter from '../../js/containers/CurrentCounter';
 describe('<CurrentCounter/> Component', () => {
 	let wrapper;
 	beforeEach(() => {
-		wrapper = shallow(<CurrentCounter />);
+		wrapper = mount(<CurrentCounter />);
 	});
 
 	it('Renders without crashing', () => {
 		const div = document.createElement('div');
 		ReactDOM.render(<CurrentCounter />, div);
 		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it('Name input works correctly', () => {
+		const input = wrapper.find('[data-test="name"]');
+		input.simulate('change', { target: { value: 'mango' } });
+		expect(input).to.have.value('mango');
+
+		const displayName = wrapper.find('[data-test="displayName"]');
+		expect(displayName).to.contain.text('mango');
 	});
 });
